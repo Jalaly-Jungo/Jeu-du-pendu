7f4899f1692ea59b907554cb732de60d5910ea88
Jeremy Jungo <Jeremy.JUNGO@cpnv.ch>
2019-01-05 14:25:02 +0100

    Interface de jeu

---
+++ Pendu/Pendu/frmChoiceCategory.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Pendu
+{
+    partial class frmChoiceCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdSport = new System.Windows.Forms.Button();
+            this.cmdCelebrity = new System.Windows.Forms.Button();
+            this.cmdFruit = new System.Windows.Forms.Button();
+            this.cmdCar = new System.Windows.Forms.Button();
+            this.cmdStone = new System.Windows.Forms.Button();
+            this.cmdIT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdSport
+            // 
+            this.cmdSport.Location = new System.Drawing.Point(188, 166);
+            this.cmdSport.Name = "cmdSport";
+            this.cmdSport.Size = new System.Drawing.Size(87, 28);
+            this.cmdSport.TabIndex = 0;
+            this.cmdSport.Text = "Sport";
+            this.cmdSport.UseVisualStyleBackColor = true;
+            // 
+            // cmdCelebrity
+            // 
+            this.cmdCelebrity.Location = new System.Drawing.Point(347, 165);
+            this.cmdCelebrity.Name = "cmdCelebrity";
+            this.cmdCelebrity.Size = new System.Drawing.Size(82, 29);
+            this.cmdCelebrity.TabIndex = 1;
+            this.cmdCelebrity.Text = "Célébrité";
+            this.cmdCelebrity.UseVisualStyleBackColor = true;
+            // 
+            // cmdFruit
+            // 
+            this.cmdFruit.Location = new System.Drawing.Point(517, 165);
+            this.cmdFruit.Name = "cmdFruit";
+            this.cmdFruit.Size = new System.Drawing.Size(76, 29);
+            this.cmdFruit.TabIndex = 2;
+            this.cmdFruit.Text = "Fruit";
+            this.cmdFruit.UseVisualStyleBackColor = true;
+            // 
+            // cmdCar
+            // 
+            this.cmdCar.Location = new System.Drawing.Point(198, 228);
+            this.cmdCar.Name = "cmdCar";
+            this.cmdCar.Size = new System.Drawing.Size(77, 28);
+            this.cmdCar.TabIndex = 3;
+            this.cmdCar.Text = "Voitures";
+            this.cmdCar.UseVisualStyleBackColor = true;
+            // 
+            // cmdStone
+            // 
+            this.cmdStone.Location = new System.Drawing.Point(347, 231);
+            this.cmdStone.Name = "cmdStone";
+            this.cmdStone.Size = new System.Drawing.Size(82, 25);
+            this.cmdStone.TabIndex = 4;
+            this.cmdStone.Text = "Caillou";
+            this.cmdStone.UseVisualStyleBackColor = true;
+            // 
+            // cmdIT
+            // 
+            this.cmdIT.Location = new System.Drawing.Point(517, 228);
+            this.cmdIT.Name = "cmdIT";
+            this.cmdIT.Size = new System.Drawing.Size(76, 28);
+            this.cmdIT.TabIndex = 5;
+            this.cmdIT.Text = "I.T.";
+            this.cmdIT.UseVisualStyleBackColor = true;
+            // 
+            // frmChoiceCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdIT);
+            this.Controls.Add(this.cmdStone);
+            this.Controls.Add(this.cmdCar);
+            this.Controls.Add(this.cmdFruit);
+            this.Controls.Add(this.cmdCelebrity);
+            this.Controls.Add(this.cmdSport);
+            this.Name = "frmChoiceCategory";
+            this.Text = "frmChoiceCategory";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdSport;
+        private System.Windows.Forms.Button cmdCelebrity;
+        private System.Windows.Forms.Button cmdFruit;
+        private System.Windows.Forms.Button cmdCar;
+        private System.Windows.Forms.Button cmdStone;
+        private System.Windows.Forms.Button cmdIT;
+    }
+}
---
+++ Pendu/Pendu/frmGame.Designer.cs
@@ -0,0 +1,444 @@
+﻿namespace Pendu
+{
+    partial class frmGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblIndex = new System.Windows.Forms.Label();
+            this.lblNomberTry = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.txtNumberTry = new System.Windows.Forms.TextBox();
+            this.lblInfoIndex = new System.Windows.Forms.Label();
+            this.cmdA = new System.Windows.Forms.Button();
+            this.cmdB = new System.Windows.Forms.Button();
+            this.cmdC = new System.Windows.Forms.Button();
+            this.cmdD = new System.Windows.Forms.Button();
+            this.cmdE = new System.Windows.Forms.Button();
+            this.cmdF = new System.Windows.Forms.Button();
+            this.cmdG = new System.Windows.Forms.Button();
+            this.cmdH = new System.Windows.Forms.Button();
+            this.cmdI = new System.Windows.Forms.Button();
+            this.cmdJ = new System.Windows.Forms.Button();
+            this.cmdK = new System.Windows.Forms.Button();
+            this.cmdL = new System.Windows.Forms.Button();
+            this.cmdM = new System.Windows.Forms.Button();
+            this.cmdN = new System.Windows.Forms.Button();
+            this.cmdO = new System.Windows.Forms.Button();
+            this.cmdP = new System.Windows.Forms.Button();
+            this.cmdQ = new System.Windows.Forms.Button();
+            this.cmdR = new System.Windows.Forms.Button();
+            this.cmdS = new System.Windows.Forms.Button();
+            this.cmdT = new System.Windows.Forms.Button();
+            this.cmdU = new System.Windows.Forms.Button();
+            this.cmdV = new System.Windows.Forms.Button();
+            this.cmdW = new System.Windows.Forms.Button();
+            this.cmdX = new System.Windows.Forms.Button();
+            this.cmdY = new System.Windows.Forms.Button();
+            this.cmdZ = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 362);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblIndex
+            // 
+            this.lblIndex.AutoSize = true;
+            this.lblIndex.Location = new System.Drawing.Point(279, 199);
+            this.lblIndex.Name = "lblIndex";
+            this.lblIndex.Size = new System.Drawing.Size(60, 20);
+            this.lblIndex.TabIndex = 1;
+            this.lblIndex.Text = "Indice :";
+            // 
+            // lblNomberTry
+            // 
+            this.lblNomberTry.AutoSize = true;
+            this.lblNomberTry.Location = new System.Drawing.Point(456, 32);
+            this.lblNomberTry.Name = "lblNomberTry";
+            this.lblNomberTry.Size = new System.Drawing.Size(204, 20);
+            this.lblNomberTry.TabIndex = 2;
+            this.lblNomberTry.Text = "Nombres d\'essais restants :";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(601, 72);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(59, 20);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "Score :";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(666, 66);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 26);
+            this.txtScore.TabIndex = 4;
+            // 
+            // txtNumberTry
+            // 
+            this.txtNumberTry.Location = new System.Drawing.Point(666, 29);
+            this.txtNumberTry.Name = "txtNumberTry";
+            this.txtNumberTry.Size = new System.Drawing.Size(100, 26);
+            this.txtNumberTry.TabIndex = 5;
+            // 
+            // lblInfoIndex
+            // 
+            this.lblInfoIndex.AutoSize = true;
+            this.lblInfoIndex.Location = new System.Drawing.Point(346, 199);
+            this.lblInfoIndex.Name = "lblInfoIndex";
+            this.lblInfoIndex.Size = new System.Drawing.Size(51, 20);
+            this.lblInfoIndex.TabIndex = 6;
+            this.lblInfoIndex.Text = "label1";
+            // 
+            // cmdA
+            // 
+            this.cmdA.Location = new System.Drawing.Point(301, 250);
+            this.cmdA.Name = "cmdA";
+            this.cmdA.Size = new System.Drawing.Size(30, 30);
+            this.cmdA.TabIndex = 7;
+            this.cmdA.Text = "A";
+            this.cmdA.UseVisualStyleBackColor = true;
+            this.cmdA.Click += new System.EventHandler(this.cmdA_Click);
+            // 
+            // cmdB
+            // 
+            this.cmdB.Location = new System.Drawing.Point(338, 250);
+            this.cmdB.Name = "cmdB";
+            this.cmdB.Size = new System.Drawing.Size(30, 30);
+            this.cmdB.TabIndex = 8;
+            this.cmdB.Text = "B";
+            this.cmdB.UseVisualStyleBackColor = true;
+            this.cmdB.Click += new System.EventHandler(this.cmdB_Click);
+            // 
+            // cmdC
+            // 
+            this.cmdC.Location = new System.Drawing.Point(372, 251);
+            this.cmdC.Name = "cmdC";
+            this.cmdC.Size = new System.Drawing.Size(30, 30);
+            this.cmdC.TabIndex = 9;
+            this.cmdC.Text = "C";
+            this.cmdC.UseVisualStyleBackColor = true;
+            // 
+            // cmdD
+            // 
+            this.cmdD.Location = new System.Drawing.Point(408, 251);
+            this.cmdD.Name = "cmdD";
+            this.cmdD.Size = new System.Drawing.Size(30, 30);
+            this.cmdD.TabIndex = 10;
+            this.cmdD.Text = "D";
+            this.cmdD.UseVisualStyleBackColor = true;
+            // 
+            // cmdE
+            // 
+            this.cmdE.Location = new System.Drawing.Point(444, 250);
+            this.cmdE.Name = "cmdE";
+            this.cmdE.Size = new System.Drawing.Size(30, 30);
+            this.cmdE.TabIndex = 11;
+            this.cmdE.Text = "E";
+            this.cmdE.UseVisualStyleBackColor = true;
+            // 
+            // cmdF
+            // 
+            this.cmdF.Location = new System.Drawing.Point(480, 251);
+            this.cmdF.Name = "cmdF";
+            this.cmdF.Size = new System.Drawing.Size(30, 30);
+            this.cmdF.TabIndex = 12;
+            this.cmdF.Text = "F";
+            this.cmdF.UseVisualStyleBackColor = true;
+            // 
+            // cmdG
+            // 
+            this.cmdG.Location = new System.Drawing.Point(516, 251);
+            this.cmdG.Name = "cmdG";
+            this.cmdG.Size = new System.Drawing.Size(30, 30);
+            this.cmdG.TabIndex = 13;
+            this.cmdG.Text = "G";
+            this.cmdG.UseVisualStyleBackColor = true;
+            // 
+            // cmdH
+            // 
+            this.cmdH.Location = new System.Drawing.Point(552, 251);
+            this.cmdH.Name = "cmdH";
+            this.cmdH.Size = new System.Drawing.Size(30, 30);
+            this.cmdH.TabIndex = 14;
+            this.cmdH.Text = "H";
+            this.cmdH.UseVisualStyleBackColor = true;
+            // 
+            // cmdI
+            // 
+            this.cmdI.Location = new System.Drawing.Point(588, 250);
+            this.cmdI.Name = "cmdI";
+            this.cmdI.Size = new System.Drawing.Size(30, 30);
+            this.cmdI.TabIndex = 15;
+            this.cmdI.Text = "I";
+            this.cmdI.UseVisualStyleBackColor = true;
+            // 
+            // cmdJ
+            // 
+            this.cmdJ.Location = new System.Drawing.Point(624, 250);
+            this.cmdJ.Name = "cmdJ";
+            this.cmdJ.Size = new System.Drawing.Size(30, 30);
+            this.cmdJ.TabIndex = 16;
+            this.cmdJ.Text = "J";
+            this.cmdJ.UseVisualStyleBackColor = true;
+            // 
+            // cmdK
+            // 
+            this.cmdK.Location = new System.Drawing.Point(660, 250);
+            this.cmdK.Name = "cmdK";
+            this.cmdK.Size = new System.Drawing.Size(30, 30);
+            this.cmdK.TabIndex = 17;
+            this.cmdK.Text = "K";
+            this.cmdK.UseVisualStyleBackColor = true;
+            // 
+            // cmdL
+            // 
+            this.cmdL.Location = new System.Drawing.Point(696, 251);
+            this.cmdL.Name = "cmdL";
+            this.cmdL.Size = new System.Drawing.Size(30, 30);
+            this.cmdL.TabIndex = 18;
+            this.cmdL.Text = "L";
+            this.cmdL.UseVisualStyleBackColor = true;
+            // 
+            // cmdM
+            // 
+            this.cmdM.Location = new System.Drawing.Point(732, 250);
+            this.cmdM.Name = "cmdM";
+            this.cmdM.Size = new System.Drawing.Size(30, 30);
+            this.cmdM.TabIndex = 19;
+            this.cmdM.Text = "M";
+            this.cmdM.UseVisualStyleBackColor = true;
+            // 
+            // cmdN
+            // 
+            this.cmdN.Location = new System.Drawing.Point(301, 286);
+            this.cmdN.Name = "cmdN";
+            this.cmdN.Size = new System.Drawing.Size(30, 30);
+            this.cmdN.TabIndex = 20;
+            this.cmdN.Text = "N";
+            this.cmdN.UseVisualStyleBackColor = true;
+            // 
+            // cmdO
+            // 
+            this.cmdO.Location = new System.Drawing.Point(337, 286);
+            this.cmdO.Name = "cmdO";
+            this.cmdO.Size = new System.Drawing.Size(30, 30);
+            this.cmdO.TabIndex = 21;
+            this.cmdO.Text = "O";
+            this.cmdO.UseVisualStyleBackColor = true;
+            // 
+            // cmdP
+            // 
+            this.cmdP.Location = new System.Drawing.Point(372, 286);
+            this.cmdP.Name = "cmdP";
+            this.cmdP.Size = new System.Drawing.Size(30, 30);
+            this.cmdP.TabIndex = 22;
+            this.cmdP.Text = "P";
+            this.cmdP.UseVisualStyleBackColor = true;
+            // 
+            // cmdQ
+            // 
+            this.cmdQ.Location = new System.Drawing.Point(408, 286);
+            this.cmdQ.Name = "cmdQ";
+            this.cmdQ.Size = new System.Drawing.Size(30, 30);
+            this.cmdQ.TabIndex = 23;
+            this.cmdQ.Text = "Q";
+            this.cmdQ.UseVisualStyleBackColor = true;
+            // 
+            // cmdR
+            // 
+            this.cmdR.Location = new System.Drawing.Point(444, 286);
+            this.cmdR.Name = "cmdR";
+            this.cmdR.Size = new System.Drawing.Size(30, 30);
+            this.cmdR.TabIndex = 24;
+            this.cmdR.Text = "R";
+            this.cmdR.UseVisualStyleBackColor = true;
+            // 
+            // cmdS
+            // 
+            this.cmdS.Location = new System.Drawing.Point(480, 286);
+            this.cmdS.Name = "cmdS";
+            this.cmdS.Size = new System.Drawing.Size(30, 30);
+            this.cmdS.TabIndex = 25;
+            this.cmdS.Text = "S";
+            this.cmdS.UseVisualStyleBackColor = true;
+            // 
+            // cmdT
+            // 
+            this.cmdT.Location = new System.Drawing.Point(516, 286);
+            this.cmdT.Name = "cmdT";
+            this.cmdT.Size = new System.Drawing.Size(30, 30);
+            this.cmdT.TabIndex = 26;
+            this.cmdT.Text = "T";
+            this.cmdT.UseVisualStyleBackColor = true;
+            // 
+            // cmdU
+            // 
+            this.cmdU.Location = new System.Drawing.Point(552, 286);
+            this.cmdU.Name = "cmdU";
+            this.cmdU.Size = new System.Drawing.Size(30, 30);
+            this.cmdU.TabIndex = 27;
+            this.cmdU.Text = "U";
+            this.cmdU.UseVisualStyleBackColor = true;
+            // 
+            // cmdV
+            // 
+            this.cmdV.Location = new System.Drawing.Point(588, 286);
+            this.cmdV.Name = "cmdV";
+            this.cmdV.Size = new System.Drawing.Size(30, 30);
+            this.cmdV.TabIndex = 28;
+            this.cmdV.Text = "V";
+            this.cmdV.UseVisualStyleBackColor = true;
+            // 
+            // cmdW
+            // 
+            this.cmdW.Location = new System.Drawing.Point(624, 286);
+            this.cmdW.Name = "cmdW";
+            this.cmdW.Size = new System.Drawing.Size(30, 30);
+            this.cmdW.TabIndex = 29;
+            this.cmdW.Text = "W";
+            this.cmdW.UseVisualStyleBackColor = true;
+            // 
+            // cmdX
+            // 
+            this.cmdX.Location = new System.Drawing.Point(660, 286);
+            this.cmdX.Name = "cmdX";
+            this.cmdX.Size = new System.Drawing.Size(30, 30);
+            this.cmdX.TabIndex = 30;
+            this.cmdX.Text = "X";
+            this.cmdX.UseVisualStyleBackColor = true;
+            // 
+            // cmdY
+            // 
+            this.cmdY.Location = new System.Drawing.Point(696, 287);
+            this.cmdY.Name = "cmdY";
+            this.cmdY.Size = new System.Drawing.Size(30, 30);
+            this.cmdY.TabIndex = 31;
+            this.cmdY.Text = "Y";
+            this.cmdY.UseVisualStyleBackColor = true;
+            // 
+            // cmdZ
+            // 
+            this.cmdZ.Location = new System.Drawing.Point(732, 287);
+            this.cmdZ.Name = "cmdZ";
+            this.cmdZ.Size = new System.Drawing.Size(30, 30);
+            this.cmdZ.TabIndex = 32;
+            this.cmdZ.Text = "Z";
+            this.cmdZ.UseVisualStyleBackColor = true;
+            // 
+            // frmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdZ);
+            this.Controls.Add(this.cmdY);
+            this.Controls.Add(this.cmdX);
+            this.Controls.Add(this.cmdW);
+            this.Controls.Add(this.cmdV);
+            this.Controls.Add(this.cmdU);
+            this.Controls.Add(this.cmdT);
+            this.Controls.Add(this.cmdS);
+            this.Controls.Add(this.cmdR);
+            this.Controls.Add(this.cmdQ);
+            this.Controls.Add(this.cmdP);
+            this.Controls.Add(this.cmdO);
+            this.Controls.Add(this.cmdN);
+            this.Controls.Add(this.cmdM);
+            this.Controls.Add(this.cmdL);
+            this.Controls.Add(this.cmdK);
+            this.Controls.Add(this.cmdJ);
+            this.Controls.Add(this.cmdI);
+            this.Controls.Add(this.cmdH);
+            this.Controls.Add(this.cmdG);
+            this.Controls.Add(this.cmdF);
+            this.Controls.Add(this.cmdE);
+            this.Controls.Add(this.cmdD);
+            this.Controls.Add(this.cmdC);
+            this.Controls.Add(this.cmdB);
+            this.Controls.Add(this.cmdA);
+            this.Controls.Add(this.lblInfoIndex);
+            this.Controls.Add(this.txtNumberTry);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblNomberTry);
+            this.Controls.Add(this.lblIndex);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmGame";
+            this.Text = "frmGame";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblIndex;
+        private System.Windows.Forms.Label lblNomberTry;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.TextBox txtNumberTry;
+        private System.Windows.Forms.Label lblInfoIndex;
+        private System.Windows.Forms.Button cmdA;
+        private System.Windows.Forms.Button cmdB;
+        private System.Windows.Forms.Button cmdC;
+        private System.Windows.Forms.Button cmdD;
+        private System.Windows.Forms.Button cmdE;
+        private System.Windows.Forms.Button cmdF;
+        private System.Windows.Forms.Button cmdG;
+        private System.Windows.Forms.Button cmdH;
+        private System.Windows.Forms.Button cmdI;
+        private System.Windows.Forms.Button cmdJ;
+        private System.Windows.Forms.Button cmdK;
+        private System.Windows.Forms.Button cmdL;
+        private System.Windows.Forms.Button cmdM;
+        private System.Windows.Forms.Button cmdN;
+        private System.Windows.Forms.Button cmdO;
+        private System.Windows.Forms.Button cmdP;
+        private System.Windows.Forms.Button cmdQ;
+        private System.Windows.Forms.Button cmdR;
+        private System.Windows.Forms.Button cmdS;
+        private System.Windows.Forms.Button cmdT;
+        private System.Windows.Forms.Button cmdU;
+        private System.Windows.Forms.Button cmdV;
+        private System.Windows.Forms.Button cmdW;
+        private System.Windows.Forms.Button cmdX;
+        private System.Windows.Forms.Button cmdY;
+        private System.Windows.Forms.Button cmdZ;
+    }
+}
